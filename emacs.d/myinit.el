30b3f60d9d97d17f82651bb64cc2e329238099d5
lainkits <protocollain@gmail.com>
2017-01-15 00:06:22 +0300

    add some more basic things to emacs

--- emacs.d/myinit.el
+++ emacs.d/myinit.el
@@ -1,6 +1,11 @@
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
+
+(setq make-backup-files         nil) ; Don't want any backup files
+(setq auto-save-list-file-name  nil) ; Don't want any .saves files
+(setq auto-save-default         nil) ; Don't want any auto saving
 
 (use-package ox-reveal
   :ensure ox-reveal)

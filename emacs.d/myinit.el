60e0fe35c01854c19a029ed5ef467b91303c6322
lain <protocollain@gmail.com>
2017-02-17 17:59:03 +0300

    myinit.el

--- emacs.d/myinit.el
+++ emacs.d/myinit.el
@@ -1,11 +1,15 @@
 
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
-(fset 'yes-or-no-p 'y-or-n-p)
+(setq user-full-name "Lain")
 
+(tool-bar-mode -1)
+(setq inhibit-startup-message t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+(prefer-coding-system 'utf-8)
 
 (use-package auto-complete
   :ensure t
@@ -15,8 +19,14 @@
     (global-auto-complete-mode t)
     ))
 
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
 (use-package seoul256-theme
   :ensure t
   :init (progn
           (setq seoul256-background 235)
           (load-theme 'seoul256 t)))
+
+

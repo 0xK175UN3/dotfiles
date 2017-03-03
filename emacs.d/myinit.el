20221700ff4251e8153d5c898ec46add0e27f349
lainkits <protocollain@gmail.com>
2017-03-04 02:58:45 +0300

    add basic setting to emacs

--- emacs.d/myinit.el
+++ emacs.d/myinit.el
@@ -1,3 +1,7 @@
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(fset 'yes-or-no-p 'y-or-n-p)
+
 (use-package ox-reveal
   :ensure ox-reveal)
 (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")

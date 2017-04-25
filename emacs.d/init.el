012caff6c9513103e84e03f03e0a7526c8650c82
lain <protocollain@gmail.com>
2017-04-25 13:16:14 +0300

    remove README from emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,9 +23,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote cabal-repl))
+ '(haslell-tags-on-save t)
  '(package-selected-packages
    (quote
-    (base16-theme smartparens web-beautify dired+ spacemacs-theme auto-complete use-package ox-reveal))))
+    (neotree base16-theme smartparens web-beautify dired+ spacemacs-theme auto-complete use-package ox-reveal))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

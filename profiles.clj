19331ba3b6b73ae1af185dd22861aa1c3efd0d50
0xk175un3 <0xk175un3@gmail.com>
2018-01-13 21:58:44 +0300

    Add profiles for leiningen

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[cider/cider-nrepl "0.15.0"]
+                  [lein-ancient "0.6.10"]
+                  [lein-exec "0.3.5"]
+                  [lein-licenses "0.2.1"]
+                  [lein-try "0.4.3"]]}}

4b916326c9b19bfd9e74038f5e72be8b4f5578a3
Artemiy Stepanov <0xk175un3@gmail.com>
2020-01-01 02:40:42 +0300

    Go modules

---
+++ go/install.sh
@@ -0,0 +1,5 @@
+go get -u golang.org/x/tools/cmd/gopls
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/gorename
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
